0fa32539f626f878e1b9d44b240fdd539e81c68f
DiegoUpgradeHub <diego.perez@bootcamp-upgrade.com>
2023-03-23 23:11:59 +0100

    V2 app-routing. He añadido Auth.Service y el CanActivate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
-import { SignUpModule } from './features/sign-up/sign-up.module';
-import { SignInModule } from './features/sign-in/sign-in.module';
-import { LoginFormComponent } from './features/login-form/login-form.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './core/guards/auth.guard'
 
 const routes: Routes = [
 
+  {
+    path: `user-profile/:id`, loadChildren: () =>
+      import('./pages/my-account/my-account.module').then(m => m.MyAccountModule),
+      canActivate: [AuthGuard]
+  },
   {
     path: `home`, loadChildren: () =>
       import('./pages/home/home.module').then(m => m.HomeModule)
@@ -14,26 +17,21 @@ const routes: Routes = [
     path: `restaurant-pizza`, loadChildren: () =>
       import('./pages/restaurant-pizza/restaurant-pizza.module').then(m => m.RestaurantPizzaModule)
   },
-  {
-    path: `my-account`, loadChildren: () =>
-      import('./pages/my-account/my-account.module').then(m => m.MyAccountModule)
-  },
-  {
-    path: `login-form`, loadChildren: () =>
-      import('./features/login-form/login-form.module').then(m => m.LoginFormModule)
-  },
+  //[Diego] No tengo claro qué es el login form. Es lo mismo que el sign-in?
+  // {
+  //   path: `login-form`, loadChildren: () =>
+  //     import('./features/login-form/login-form.module').then(m => m.LoginFormModule)
+  // },
   {
     path: `sign-in`, loadChildren: () =>
       import('./features/sign-in/sign-in.module').then(m => m.SignInModule)
   },
   {
     path: `sign-up`, loadChildren: () =>
-      import('./features/sign-up/sign-up.module').then(m => m.SignUpModule)
+      import('./features/sign-up/sign-up.module').then(m => m.SignUpModule),
+      canActivate: [AuthGuard]
   },
   { path: ``, redirectTo: `home`, pathMatch: `full` }
-
-
-
 
 ];
 
