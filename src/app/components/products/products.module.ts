eda5ed694584eeb7dc9a3334f79eb001f2a0490f
nikitero <nikitavasiljevs@gmail.com>
2023-03-24 17:52:17 +0100

    creamos modules separados para products/users

---
+++ src/app/components/products/products.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CreateProductComponent } from './create-product/create-product.component';
+import { EditProductComponent } from './edit-product/edit-product.component';
+import { DeleteProductComponent } from './delete-product/delete-product.component';
+
+
+@NgModule({
+  declarations: [
+    CreateProductComponent,
+    EditProductComponent,
+    DeleteProductComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    CreateProductComponent,
+    EditProductComponent,
+    DeleteProductComponent
+  ]
+})
+export class ProductsModule { }
---
+++ src/app/components/users/users.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CreateUserComponent } from './create-user/create-user.component';
+import { EditUserComponent } from './edit-user/edit-user.component';
+import { DeleteUserComponent } from './delete-user/delete-user.component';
+
+@NgModule({
+  declarations: [
+    CreateUserComponent,
+    EditUserComponent,
+    DeleteUserComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    CreateUserComponent,
+    EditUserComponent,
+    DeleteUserComponent
+  ]
+})
+export class UsersModule { }
