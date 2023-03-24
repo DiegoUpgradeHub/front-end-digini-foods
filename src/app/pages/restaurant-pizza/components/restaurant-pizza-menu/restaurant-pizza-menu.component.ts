0fad74efc7e65f7c884f41b3188c22f4d5c8e821
nikitero <nikitavasiljevs@gmail.com>
2023-03-24 23:13:24 +0100

    Implemento el accordeon en restaurant-pizza component

--- src/app/pages/restaurant-pizza/components/restaurant-pizza-menu/restaurant-pizza-menu.component.ts
+++ src/app/pages/restaurant-pizza/components/restaurant-pizza-menu/restaurant-pizza-menu.component.ts
@@ -1,4 +1,7 @@
+import { ProductApiResponse } from './../../../../core/models/product';
 import { Component } from '@angular/core';
+import { Product } from 'src/app/core/models/product';
+import { ProductsService } from 'src/app/core/services/products.service';
 
 @Component({
   selector: 'app-restaurant-pizza-menu',
@@ -6,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./restaurant-pizza-menu.component.scss']
 })
 export class RestaurantPizzaMenuComponent {
+
+  productList: Product[] = [];
+  plates: Product[] = [];
+  drinks: Product[] = [];
+
+  constructor (private productsService: ProductsService) {}
+
+  ngOnInit():void{
+    this.getProducts()
+}
+
+  private getProducts():void{
+
+    this.productsService.getProducts().subscribe((response)=>{
+      this.productList = response;
+      this.plates = this.productList.filter(product => product.category === 'Pizzas' )
+      this.drinks = this.productList.filter(drink => drink.category === 'Bebidas' )
+      console.log(this.plates);
+    });
+
+  }
+
 
 }
