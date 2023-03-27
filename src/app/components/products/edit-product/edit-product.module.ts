ccdb4f0793c08eb9ee7871ea936342d5144dec9b
nikitero <nikitavasiljevs@gmail.com>
2023-03-27 14:37:47 +0200

    añado rutas al menu

--- src/app/components/products/edit-product/edit-product.module.ts
+++ src/app/components/products/edit-product/edit-product.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { EditProductRoutingModule } from './edit-product-routing.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    EditProductRoutingModule
   ]
 })
 export class EditProductModule { }
