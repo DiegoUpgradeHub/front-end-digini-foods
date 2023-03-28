d99166a231aaf70fd27d7b249ecea29610a98723
DiegoUpgradeHub <diego.perez@bootcamp-upgrade.com>
2023-03-28 23:12:45 +0200

    modificaciones en app.scss para eliminar el gap entre menu y app-routing. Modificación del my-account para mostrar datos, daba error

--- src/app/pages/my-account/my-account.component.ts
+++ src/app/pages/my-account/my-account.component.ts
@@ -18,7 +18,7 @@ export class MyAccountComponent implements OnInit {
   ) {
     let id = localStorage.getItem('_id'); //Recogemos el id del local storage
     this.authService.getUserProfile(id).subscribe(res => { //Llamamos a la función para obtener los datos del usuario
-      this.currentUser = res.msg;
+      this.currentUser = res;
       console.log(this.currentUser);
     })
   }
