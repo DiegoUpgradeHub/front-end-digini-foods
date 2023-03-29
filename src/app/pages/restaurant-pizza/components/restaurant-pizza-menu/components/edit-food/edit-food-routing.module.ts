ea45641af5416ce3d25e07a1f005cc48fa3c7c11
nikitero <nikitavasiljevs@gmail.com>
2023-03-29 13:00:33 +0200

    Editar productos desarrollado

--- src/app/pages/restaurant-pizza/components/restaurant-pizza-menu/components/edit-food/edit-food-routing.module.ts
+++ src/app/pages/restaurant-pizza/components/restaurant-pizza-menu/components/edit-food/edit-food-routing.module.ts
@@ -1,15 +1,16 @@
-import { EditProductComponent } from './edit-product.component';
+import { EditFoodComponent } from './edit-food.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
 
-
-
+  {
+    path: '', component: EditFoodComponent
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class EditProductRoutingModule { }
+export class EditFoodRoutingModule { }
